2c2a2aa1053c2a9ba2b9364e13f6e0eec58933e8
mazhenhua <mazh@51guaihuai.cn>
2019-04-26 18:50:21 +0800

    提交发布脚本

---
+++ start.sh
@@ -0,0 +1,13 @@
+cd /sentinel-dashboard
+
+mvn clean package -U -e -Dmaven.normal.skip=true  -Dmaven.test.skip=true
+
+cd target/
+
+cp ../Dockerfile ./Dockerfile
+
+docker build --pull=true -t hub.mzh.com:sentinel-dashboard ./
+
+docker stop sentinel-dashboard
+
+docker run -d --name=sentinel-dashboard hub.mzh.com:sentinel-dashboard
