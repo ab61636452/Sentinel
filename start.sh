5b8240b86ad98b13c7c21fcb39130b47185a943e
mazhenhua <mazh@51guaihuai.cn>
2019-04-26 19:20:01 +0800

    修改脚本

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 cd sentinel-dashboard
 
+
+
 mvn clean package -U -e -Dmaven.normal.skip=true  -Dmaven.test.skip=true
 
 cd target/
@@ -8,6 +10,6 @@ cp ../Dockerfile ./Dockerfile
 
 docker build --pull=true -t hub.mzh.com:sentinel-dashboard ./
 
-docker stop sentinel-dashboard
+docker rm -f sentinel-dashboard
 
 docker run -d --name=sentinel-dashboard hub.mzh.com:sentinel-dashboard
